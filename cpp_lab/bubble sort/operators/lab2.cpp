dc7ec5d117e38811ccdc384a35c5e5fa9c668d25
Данила Кравченко <88050202+KravchaDev@users.noreply.github.com>
2022-11-21 16:24:12 +0800

    Update lab2.cpp

--- cpp_lab/bubble sort/operators/lab2.cpp
+++ cpp_lab/bubble sort/operators/lab2.cpp
@@ -1,20 +1,21 @@
 #include <math.h>
+#include <stdio.h>
 
 int main() {
-    float a, b, summ, razn, chast, proiz;
+  float a, b, summ, razn, chast, proiz;
 
-    printf("Vvedite pervoe i vtoroe chislo");
-    scanf_s("%f%f", &a, &b);
+  printf("Vvedite pervoe i vtoroe chislo: \n");
+  scanf("%f%f", &a, &b);
 
-    summ = a + b;
-    razn = a - b;
-    chast = (a * a)/(b * b);
-    proiz = a * b;
+  summ = a + b;
+  razn = a - b;
+  chast = (a * a) / (b * b);
+  proiz = a * b;
 
-    printf("Summa = %.2f", summ);
-    printf("\nRaznost = %.2f", razn);
-    printf("\nChast = %.2f", chast);
-    printf("\nProiz = %.2f", proiz);
-    
-    return 0;
+  printf("Summa = %.2f", summ);
+  printf("\nRaznost = %.2f", razn);
+  printf("\nChast = %.2f", chast);
+  printf("\nProiz = %.2f", proiz);
+
+  return 0;
 }
